e56cd3db7f9d987acd691ce6f6446fb93a35842e
nzoLogic <nzoLogic@gmail.com>
2017-08-03 22:37:41 -0700

    saving newVisitor

--- src/utils/NewVisitor.js
+++ src/utils/NewVisitor.js
@@ -1,6 +1,5 @@
 function isNewVisitor(){
   let hasVisited = window.localStorage.getItem('hasVisited')
-  console.log(hasVisited)
   if(hasVisited) return false 
   else{
     window.localStorage.setItem('hasVisited', true)
